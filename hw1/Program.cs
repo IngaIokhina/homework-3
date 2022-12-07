08b7a4c85cd98e7a2cd7db5131c7305a3c0fc2ad
Inga Iokhina <iiokhina95@mail.ru>
2022-12-07 14:58:00 +0400

    add hw 1, 2, 3, gitignore

---
+++ hw1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное 
+число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.Write("Enter the number: ");
+string? number = Console.ReadLine();
+void GetNumber(string number)
+{
+    if (number[0]==number[4] || number[1]==number[3])
+    {
+        Console.Write("The number is a palindrome ");
+    }
+    else
+    {
+        Console.WriteLine("Number is NOT a palindrome");
+    }
+}
+if (number!.Length == 5)
+{
+  GetNumber(number);
+}
+else Console.WriteLine("Enter another number");
---
+++ hw2/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек \
+и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+
+d=sqrt(x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2);
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("You didn't enter a number. Enter the number");
+        }
+    }
+
+    return result;
+}
+
+double GetDistanse (int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    return Math.Sqrt(Math.Pow (x2 - x1,2)+ Math.Pow (y2 - y1,2) + Math.Pow(z2 - z1,2));
+}
+
+int x1 = GetNumber ("Enter the x coordinate for the first point");
+int y1 = GetNumber ("Enter the y coordinate for the first point");
+int z1 = GetNumber ("Enter the z coordinate for the first point");
+int x2 = GetNumber ("Enter the x coordinate for the second point");
+int y2 = GetNumber ("Enter the y coordinate for the second point");
+int z2 = GetNumber ("Enter the z coordinate for the second point");
+
+double distanse = GetDistanse (x1, x2, y1, y2, z1, z2);
+
+Console.WriteLine (distanse);
